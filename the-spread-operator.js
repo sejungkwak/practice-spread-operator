a92a457c95485f3d384345fd6bf8428249d27903
Sejung Kwak <kwak.sejung@gmail.com>
2022-06-25 19:34:25 +0000

    No spread operator, Copying an array, Copying an object, Copying only part of an array/object

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -5,8 +5,26 @@
 
 // No spread operator
 
+let arr1 = [1, 2, 3]
+let arr2 = arr1;
+arr2.push(4)
+console.log(arr1, arr2);
+
 // Copying an array
+
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
+arr4.push(7);
+console.log(arr3, arr4);
 
 // Copying an object
 
+let obj1 = { a: 1, b: 2, c: 3 };
+let obj2 = { ...obj1, d: 4 };
+let obj3 = { ...obj1, b: 5 };
+console.log(obj1, obj2, obj3);
+
 // Copying only part of an array/object
+
+let arr5 = [...arr1, {...obj1}, ...arr3, 'x', 'y', 'z'];
+console.log(arr5);
